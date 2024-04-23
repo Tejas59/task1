0330c7f5ba779d4345df2710bdd4c54dbe311304
Tejas Vaidya <57625719+Tejas59@users.noreply.github.com>
2024-04-23 10:43:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["https://authmernfrontend-git-main-tejas59s-projects.vercel.app/"],
     methods:["GET", "post"],
     credentials: true
 }));
