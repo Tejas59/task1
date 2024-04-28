155ef56a8837854e0d7df93f4a13997206c30e82
Tejas59 <57625719+Tejas59@users.noreply.github.com>
2024-04-28 22:50:51 +0530

    updated index.js file

--- server/index.js
+++ server/index.js
@@ -68,7 +68,17 @@ app.post("/", async (req, res) => {
       console.log('JWT:', token);
       res.cookie("token", token);
       return res.json({ status: "success", role: user.role, name: user.name });
-    } else {
+    }else if (user.lastLoginAttempt === null) {
+      const now = new Date();
+      const nullUser = await UserModel.findOneAndUpdate(
+        { email },
+        { $inc: { loginAttempts: 1 }, lastLoginAttempt: now },
+        { new: true }
+      );
+    
+      return res.status(401).json({ message: "Incorrect email or password. Please try again." });
+    }
+    else {
       const now = new Date();
       const twentyFourHoursAgo = new Date(now.getTime() - 24 * 60 * 60 * 1000);
       const updatedUser = await UserModel.findOneAndUpdate(
